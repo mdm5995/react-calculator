877730d23bed30aadf0c5c81f5eaf0e917694b76
Morgan <mdm5995@gmail.com>
2023-04-05 12:40:46 -0400

    Remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-			<div className="container">
-			</div>
-		</div>
+    <div className="App"></div>
   );
 }
 
