f301574b8eff1759b7b26a4b79c6f3b3a2dabea3
Morgan <mdm5995@gmail.com>
2023-04-05 13:10:46 -0400

    Create calculatorButtons obj and divs

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import './App.css';
 
+const calculatorButtons = {
+	clear: 'AC',
+	divide: '/',
+	multiply: '*',
+	seven: 7,
+	eight: 8,
+	nine: 9,
+	minus: '-',
+	four: 4,
+	five: 5,
+	six: 6,
+	plus: '+',
+	equals: '=',
+	one: 1,
+	two: 2,
+	three: 3,
+}
+
 function App() {
   return (
     <div className="App">
 			<div className="grid">
 				<div id="display" />
-				<div id="clear" />
-				<div id="divide" />
-				<div id="multiply" />
-				<div id="seven" />
-				<div id="eight" />
-				<div id="nine" />
-				<div id="minus" />
-				<div id="four" />
-				<div id="five" />
-				<div id="six" />
-				<div id="plus" />
-				<div id="one" />
-				<div id="two" />
-				<div id="three" />
-				<div id="equals" />
-				<div id="zero" />
-				<div id="decimal" />
+				{Object.keys(calculatorButtons).map((key) => {
+					return (
+						<div id={key} value={calculatorButtons[key]}>{calculatorButtons[key]}</div>
+					)
+				})}
 			</div>
 		</div>
   );
