48310243feea349cbbf2fde75d8b81635463d7ba
Morgan <mdm5995@gmail.com>
2023-04-05 16:25:59 -0400

    Create initial display logic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
-import './App.css';
+import { useState, useEffect } from 'react'
+import './App.css'
+import { evaluate } from 'mathjs'
 
 const calculatorButtons = {
+	// these affect the display order of the calculator. 
+	// perhaps not ideal
 	clear: 'AC',
 	divide: '/',
 	multiply: '*',
@@ -21,13 +25,32 @@ const calculatorButtons = {
 }
 
 function App() {
+
+	const [calculation, setCalculation] = useState([]);
+	const [display, setDisplay] = useState('initial display');
+
+	// on button press, relevant glyph is pushed to "calculation" array
+	// on equals button press, math.evaluate(calculation.concat())
+	// on AC button press, calculation = '';
+	// for */+ operators, include check that previous glyph is not operator, 
+	// // -(minus) operator excluded because can be negative number
+	// if it is operator, replace it with new button press
+
+	const handleClick = (event) => {
+		setCalculation([...calculation, event.target.value]);
+	};
+
+	useEffect(() => {
+		setDisplay(calculation.concat());
+	}, [calculation]);
+
   return (
     <div className="App">
 			<div className="grid">
-				<div id="display">display</div>
+				<div id="display">{display}</div>
 				{Object.keys(calculatorButtons).map((key) => {
 					return (
-						<button id={key} value={calculatorButtons[key]}>{calculatorButtons[key]}</button>
+						<button id={key} onClick={handleClick} value={calculatorButtons[key]}>{calculatorButtons[key]}</button>
 					)
 				})}
 			</div>
