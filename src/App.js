333a55a3b9821597af428acc53f18d2ade778315
Morgan <mdm5995@gmail.com>
2023-04-05 12:52:03 -0400

    Add HTML grid skeleton

--- src/App.js
+++ src/App.js
@@ -2,7 +2,28 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+			<div className="grid">
+				<div id="display" />
+				<div id="clear" />
+				<div id="divide" />
+				<div id="multiply" />
+				<div id="seven" />
+				<div id="eight" />
+				<div id="nine" />
+				<div id="minus" />
+				<div id="four" />
+				<div id="five" />
+				<div id="six" />
+				<div id="plus" />
+				<div id="one" />
+				<div id="two" />
+				<div id="three" />
+				<div id="equals" />
+				<div id="zero" />
+				<div id="decimal" />
+			</div>
+		</div>
   );
 }
 
