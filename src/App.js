a8bef90d24c7963a81cb06f80326a72b6d3bc042
Morgan <mdm5995@gmail.com>
2023-06-25 16:38:18 -0400

    Refactor decimal handling to use 'canDecimal' bool

--- src/App.js
+++ src/App.js
@@ -27,13 +27,17 @@ const calculatorButtons = {
 function App() {
 
 	const [calculation, setCalculation] = useState([]);
+	// Tracks legality of decimal point
+	// changes false when a decimal is added
+	// changes to true when the next operator, AC or = is pressed
+	const [canDecimal, setCanDecimal] = useState(true);
 	const [display, setDisplay] = useState('initial display');
 	const [answer, setAnswer] = useState('');
 
 	// on button press, relevant glyph is pushed to "calculation" array
 	// on equals button press, math.evaluate(calculation.concat())
 	// on AC button press, calculation = '';
-	// for */+ operators, include check that previous glyph is not operator, 
+	// for */+. operators, include check that previous glyph is not operator, 
 	// // -(minus) operator excluded because can be negative number
 	// if it is operator, replace it with new button press
 
@@ -43,25 +47,34 @@ function App() {
 			return regex.test(string);
 		}
 		const isOperator = (string) => {
-			const regex = /[+-/\*]/;
+			const regex = /[+-/\*\.]/;
 			return regex.test(string);
 		}
 
 		if (event.target.value === 'AC') {
 			setCalculation([]);
+			setCanDecimal(true);
 			return;
 		}
 
 		if (isDigit(event.target.value)) {
 			setCalculation([...calculation, event.target.value]);
-				return;
+			return;
 		} 
 
-		if (event.target.value === '.' 
-			&& (calculation.length === 0 || isOperator(calculation[calculation.length - 1]))) {
-			setCalculation([...calculation, '0.']);
+		if (event.target.value === '.') {
+			if (canDecimal && isOperator(calculation.at(-1))) {
+				setCalculation([...calculation, '0.']);
+				setCanDecimal(false);
+				return;
+			} else if (canDecimal) {
+				setCalculation([...calculation, '.']);
+				setCanDecimal(false);
+				return;
+			}
+			// illegal decimal
 			return;
-		}
+		} 
 
 		// operator logic
 		if (isOperator(event.target.value)) {
@@ -69,21 +82,24 @@ function App() {
 			// no operator possible except '-' as negative sign
 			if (calculation.length === 0 && event.target.value === '-') {
 				setCalculation([...calculation, event.target.value]);
+				setCanDecimal(true);
 				return;
 			}
 			// if calc[-1] === operator && calc[-2] isDigit, 
 			// append minus, otherwise append nothing
 			if (
-				isOperator(calculation[calculation.length - 1]) 
-				&& isDigit(calculation[calculation.length - 2]) 
+				isOperator(calculation.at(-1)) 
+				&& isDigit(calculation.at(-2)) 
 				&& event.target.value === '-'
 			) {
 				setCalculation([...calculation, event.target.value]);
+				setCanDecimal(true);
 				return;
 			}
 			// if calc[-1] !== operator, append operator
 			if (isDigit(calculation[calculation.length - 1])) {
 				setCalculation([...calculation, event.target.value]);
+				setCanDecimal(true);
 				return;
 			}
 		}
